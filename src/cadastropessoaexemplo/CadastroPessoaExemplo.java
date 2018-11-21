8479f08bd2a5ef9878a18b3dff9449344eb111ed
Monique Rocha <monique.rocha@sistematic.net.br>
2018-11-21 11:17:03 -0400

    - chamando classe de ViewCadastro para classe do projeto cadastropessoaexemplo

--- src/cadastropessoaexemplo/CadastroPessoaExemplo.java
+++ src/cadastropessoaexemplo/CadastroPessoaExemplo.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cadastropessoaexemplo;
+
+import view.ViewCadastro;
 
 /**
  *
@@ -15,7 +13,8 @@ public class CadastroPessoaExemplo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ViewCadastro telaCadastro = new ViewCadastro();
+        telaCadastro.setVisible(true);
     }
     
 }
